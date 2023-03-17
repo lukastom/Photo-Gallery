ba10bcc02ad3f2a8b781d22268c26b390ce11463
lukastom <posli.posli@email.cz>
2023-03-17 21:08:03 +0100

    0.0.5

--- src/main/java/cz/programujes/lukas/photo/gallery/GalleryController.java
+++ src/main/java/cz/programujes/lukas/photo/gallery/GalleryController.java
@@ -1,9 +1,12 @@
 package cz.programujes.lukas.photo.gallery;
 
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
@@ -49,10 +52,14 @@ public class GalleryController {
         }
     }
 
-    //POST method
+    //POST method + validation of JSON input (invalid throws 400 Bad Request)
     @PostMapping("/gallery")
-    public Photo uploadPhoto (@RequestBody Photo photo) {
+    //For just text input (prototyping): uploadPhoto (@RequestBody @Valid Photo photo)
+    public Photo uploadPhoto (@RequestPart("data") MultipartFile file) throws IOException {
+        Photo photo = new Photo();
         photo.setId(UUID.randomUUID().toString());
+        photo.setFileName(file.getOriginalFilename());
+        photo.setData(file.getBytes());
         db.put(photo.getId(), photo);
         return photo;
     }
--- src/main/java/cz/programujes/lukas/photo/gallery/Photo.java
+++ src/main/java/cz/programujes/lukas/photo/gallery/Photo.java
@@ -1,11 +1,17 @@
 package cz.programujes.lukas.photo.gallery;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public class Photo {
 
     private String id;
+
+    //variable + a rule for validation (not empty)
+    @NotEmpty
     private String fileName;
 
     //raw data
+    private byte[] data;
 
     //2 constructors, overloading
     public Photo() {
@@ -24,11 +30,19 @@ public class Photo {
         return fileName;
     }
 
+    public byte[] getData() {
+        return data;
+    }
+
     public void setId(String id) {
         this.id = id;
     }
 
     public void setFileName(String fileName) {
         this.fileName = fileName;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
     }
 }

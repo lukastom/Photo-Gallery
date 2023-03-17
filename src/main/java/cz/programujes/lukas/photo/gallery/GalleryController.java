022d020b8775e243b483919343234aef074cd115
lukastom <posli.posli@email.cz>
2023-03-17 10:11:08 +0100

    0.0.2

--- src/main/java/cz/programujes/lukas/photo/gallery/GalleryController.java
+++ src/main/java/cz/programujes/lukas/photo/gallery/GalleryController.java
@@ -1,26 +1,55 @@
 package cz.programujes.lukas.photo.gallery;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class GalleryController {
 
-    /*database of photos
-      • List.of – immutable list (for mutable List use ArrayList)
-     */
-    private List<Photo> db = List.of(new Photo("1", "photo1.jpg"));
+    //database of photos
+    private Map<String, Photo> db = new HashMap<>() {{
+        put("1", new Photo("1", "photo1.jpg"));
+    }};
 
+    //GET methods
     @GetMapping("/")
     public String hello () {
         return "Hello";
     }
 
     @GetMapping("/gallery")
-    public List<Photo> getPhotos () {
-        return db;
+    public Collection<Photo> getPhotos () {
+        return db.values();
     }
+
+    @GetMapping("/gallery/{id}")
+    public Photo getPhoto (@PathVariable String id) {
+        Photo photo = db.get(id);
+        // If photo not found, throw 404
+        if (photo == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return photo;
+    }
+
+    //DELETE method
+    @DeleteMapping ("/gallery/{id}")
+    public void deletePhoto (@PathVariable String id) {
+        Photo photo = db.remove(id);
+        // If photo not found, throw 404
+        if (photo == null){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
 
 }

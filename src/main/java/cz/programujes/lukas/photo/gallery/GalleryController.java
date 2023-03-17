c984133714fffd325ee5b3a6eab58679fa99f33b
lukastom <posli.posli@email.cz>
2023-03-17 14:00:41 +0100

    0.0.3

--- src/main/java/cz/programujes/lukas/photo/gallery/GalleryController.java
+++ src/main/java/cz/programujes/lukas/photo/gallery/GalleryController.java
@@ -1,22 +1,20 @@
 package cz.programujes.lukas.photo.gallery;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 public class GalleryController {
 
-    //database of photos
+    //mock database of photos for prototyping
     private Map<String, Photo> db = new HashMap<>() {{
+        //the key is just for retrieving, the actual data are in value
         put("1", new Photo("1", "photo1.jpg"));
     }};
 
@@ -49,6 +47,14 @@ public class GalleryController {
         if (photo == null){
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
+    }
+
+    //POST method
+    @PostMapping("/gallery")
+    public Photo uploadPhoto (@RequestBody Photo photo) {
+        photo.setId(UUID.randomUUID().toString());
+        db.put(photo.getId(), photo);
+        return photo;
     }
 
 
